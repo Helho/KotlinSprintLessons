3d2d28a965c871eeb61d158ea9102ccaf1db8d20
hellh <helho.bhood@gmail.com>
2025-07-02 14:40:03 +0300

    KS-1-1 real task

---
+++ app/src/main/kotlin/lesson_1/lesson_1_real_1.kt
@@ -0,0 +1,11 @@
+package org.example.app.lesson_1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val greetingMessage: String = "Thanks for your purchase!"
+
+    println("Number of orders: $numberOfOrders")
+    println(greetingMessage)
+
+}
