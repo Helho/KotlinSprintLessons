40d15b1a7afbb0ebdc218242f7136dbe32732f75
hellh <helho.bhood@gmail.com>
2025-07-02 13:06:24 +0300

    KS-1-3 real task 3

---
+++ app/src/main/kotlin/lesson_1/lesson_1_3.kt
@@ -0,0 +1,20 @@
+package org.example.app.lesson_1
+
+fun main() {
+
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println("The year of Yuri Gagarin's flight into spase - $year!")
+    //println("Hour: $hour")
+    //println("Minutes: $minute")
+
+    hour = 10
+    minute = 55
+
+    print("Time: $hour:$minute")
+
+
+
+}
