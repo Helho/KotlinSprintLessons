92d93a7ff519216c341faa125f75666c4b3488c1
hellh <helho.bhood@gmail.com>
2025-07-05 22:34:29 +0300

    KS-1-5 real task

--- app/src/main/kotlin/lesson_1/lesson_1_5.kt
+++ app/src/main/kotlin/lesson_1/lesson_1_5.kt
@@ -3,12 +3,15 @@ package org.example.app.lesson_1
 fun main() {
 
     val totalSeconds: Short = 6480
-    val hours = totalSeconds / 3600
-    val remainingSecondsAfterHours = totalSeconds % 3600
-    val minutes = remainingSecondsAfterHours / 60
-    val seconds = remainingSecondsAfterHours % 60
-    val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    val hours = totalSeconds / SECONDS_IN_HOUR
+    val remainingSecondsAfterHours = totalSeconds % SECONDS_IN_HOUR
+    val minutes = remainingSecondsAfterHours / SECONDS_IN_MINUTE
+    val seconds = remainingSecondsAfterHours % SECONDS_IN_MINUTE
+    val formattedTime = "%02d:%02d:%02d".format(hours, minutes, seconds)
 
     println("Time in space: $formattedTime")
 
 }
+
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
