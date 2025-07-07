2a8ad20f03ed6de157606756fcae381d44b68949
hellh <helho.bhood@gmail.com>
2025-07-07 11:32:40 +0300

    KS-3-3 real task

---
+++ app/src/main/kotlin/lesson_3/lesson_3_3.kt
@@ -0,0 +1,17 @@
+package org.example.app.lesson_3
+
+fun main() {
+    val numberOfMultiply = 6
+
+    println("""
+        ${numberOfMultiply} x 1 = ${numberOfMultiply * 1}
+        ${numberOfMultiply} x 2 = ${numberOfMultiply * 2}
+        ${numberOfMultiply} x 3 = ${numberOfMultiply * 3}
+        ${numberOfMultiply} x 4 = ${numberOfMultiply * 4}
+        ${numberOfMultiply} x 5 = ${numberOfMultiply * 5}
+        ${numberOfMultiply} x 6 = ${numberOfMultiply * 6}
+        ${numberOfMultiply} x 7 = ${numberOfMultiply * 7}
+        ${numberOfMultiply} x 8 = ${numberOfMultiply * 8}
+        ${numberOfMultiply} x 9 = ${numberOfMultiply * 9}
+    """.trimIndent())
+}
