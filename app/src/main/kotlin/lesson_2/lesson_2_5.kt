d561207eb43444d1749fb250722aaffd53776c68
hellh <helho.bhood@gmail.com>
2025-07-07 11:23:20 +0300

    KS-2-5 real task

--- app/src/main/kotlin/lesson_2/lesson_2_5.kt
+++ app/src/main/kotlin/lesson_2/lesson_2_5.kt
@@ -1,15 +1,19 @@
 package org.example.app.lesson_2
 
+import java.util.Locale
 import kotlin.math.pow
 
 fun main() {
 
     val initialAmount = 70_000.0
-    val annualRate = 16.7 / 100
+    val annualRate = 16.7
+    val percentageBase = annualRate / PERCENT_FACTOR
     val years = 20
 
-    val finalAmount = initialAmount * (1 + annualRate).pow(years.toDouble())
+    val finalAmount = initialAmount * (1 + percentageBase).pow(years.toDouble())
 
-    println("Сумма вклада через $years лет: %.3f".format(finalAmount))
+    println("Сумма вклада через $years лет: %.3f".format(Locale.US, finalAmount))
 
 }
+
+const val PERCENT_FACTOR = 100
