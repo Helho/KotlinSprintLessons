128b60d70f7419b6b3a0b243ced09a29026a9fdd
hellh <helho.bhood@gmail.com>
2025-07-10 14:16:38 +0300

    KS-5-3 real task

---
+++ app/src/main/kotlin/lesson_5/lesson_5_3.kt
@@ -0,0 +1,28 @@
+package lesson_5
+
+fun main() {
+
+    val secretNumber1 = 1
+    val secretNumber2 = 40
+
+    println("Угадайте два числа от 0 до 42 и выиграйте приз!")
+    print("Введите первое число: ")
+    val userNumber1 = readln().toIntOrNull() ?: -1
+    print("Введите второе число: ")
+    val userNumber2 = readln().toIntOrNull() ?: -1
+
+    val guessedBoth = (userNumber1 == secretNumber1 && userNumber2 == secretNumber2) ||
+            (userNumber1 == secretNumber2 && userNumber2 == secretNumber1)
+
+    val guessedOne = (userNumber1 == secretNumber1 || userNumber1 == secretNumber2) ||
+            (userNumber2 == secretNumber1 || userNumber2 == secretNumber2)
+
+    when {
+        guessedBoth -> println("Поздравляем! Вы выиграли главный приз!")
+        guessedOne -> println("Вы выиграли утешительный приз!")
+        else -> println("Неудача!")
+    }
+
+    println("Для победы нужно было угадать: $secretNumber1 и $secretNumber2")
+
+}
