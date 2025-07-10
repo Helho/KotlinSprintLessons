04454e6c12fc7ec44aefff9ef149ce923f680d3d
hellh <helho.bhood@gmail.com>
2025-07-10 13:01:37 +0300

    KS-4-1 real task

--- app/src/main/kotlin/lesson_4/lesson_4_1.kt
+++ app/src/main/kotlin/lesson_4/lesson_4_1.kt
@@ -2,13 +2,14 @@ package lesson_4
 
 fun main() {
 
-    val totalTables = 13
     val bookedToday = 13
     val bookedTomorrow = 9
 
-    val availableToday = bookedToday < totalTables
-    val availableTomorrow = bookedTomorrow < totalTables
+    val availableToday = bookedToday < TOTAL_TABLES
+    val availableTomorrow = bookedTomorrow < TOTAL_TABLES
 
     println("Доступность столиков на сегодня: $availableToday,\nДоступность столиков на завтра: $availableTomorrow")
 
 }
+
+const val TOTAL_TABLES = 13
